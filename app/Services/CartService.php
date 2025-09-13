0e0e332871258657f0b1bad5a5f6333be35e1941
HermesSantos <hermessantos995@gmail.com>
2025-09-13 12:28:29 -0300

    Refactor CartService to improve readability and maintainability

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -5,29 +5,40 @@ namespace App\Services;
 
 class CartService
 {
+    private const TEN_PERCENT_DISCOUNT = 0.1;
+    private const ONE_PERCENT_DISCOUNT = 0.01;
+
     public function calculateTaxes (array $data) {
 
-        $amount = 0;
-        foreach ($data['produtos'] as $product) {
-            $amount += $product['valor'] * $product['quantidade'];
-        }
+        $amount = $this->calculateAmount($data);
 
         if($data['metodo_pagamento'] == 'PIX') {
             return $this->pixPayment($amount);
         }
+
         if($data['metodo_pagamento'] == 'CARTAO_CREDITO') {
             return $this->creditCardPayment($amount, $data['parcelas']);
         }
     }
 
     private function pixPayment (int $amount) {
-        return round($amount - 0.1 * $amount, 2);
+        return round($amount - self::TEN_PERCENT_DISCOUNT * $amount, 2);
     }
 
     private function creditCardPayment (int $amount, int $installments) {
 
-        if($installments == 1) return round($amount - 0.1 * $amount, 2);
+        if($installments == 1) return round($amount - self::TEN_PERCENT_DISCOUNT * $amount, 2);
 
-        return round($amount * pow((1 + 0.01), $installments), 2);
+        return round($amount * pow((1 + self::ONE_PERCENT_DISCOUNT), $installments), 2);
+    }
+
+    private function calculateAmount (array $data) {
+        $amount = 0;
+
+        foreach ($data['produtos'] as $product) {
+            $amount += $product['valor'] * $product['quantidade'];
+        }
+
+        return $amount;
     }
 }
