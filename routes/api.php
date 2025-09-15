ec180f7cb519f44b5bcb3ac5ecfb53bd46973476
HermesSantos <hermessantos995@gmail.com>
2025-09-15 00:55:17 -0300

    Add get products route

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json(['message' => 'Api is working']);
 });
 
+Route::get('/get-products', [ProductsController::class, 'getProducts']);
 Route::post('/calculate-cart-taxes', [CartController::class, 'calculateCartTaxes']);
